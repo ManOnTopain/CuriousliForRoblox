ee8d293b7d2ae093b67ea6714cd1e9c529c36930
ManOnTopain <116823830+ManOnTopain@users.noreply.github.com>
2022-12-26 19:20:39 -0400

    Update 613957114.lua

---
+++ Custom Modules/613957114.lua
@@ -0,0 +1,175 @@
+
+local function Button(Tab, Name, Call)
+	local template_buton = Instance.new("TextButton")
+	template_buton.Font = Enum.Font.SourceSansBold
+	template_buton.Text = Name
+	template_buton.TextColor3 = Color3.new(0, 0, 0)
+	template_buton.TextScaled = true
+	template_buton.TextSize = 14
+	template_buton.TextWrapped = true
+	template_buton.BackgroundColor3 = Color3.new(0.333333, 0.345098, 0.12549)
+	--//template_buton.Position = UDim2.new(-0.0187500007, 0, 0.00350099197, 0)
+	template_buton.Size = UDim2.new(0.925000012, 0, 0.0291114338, 0)
+	template_buton.Visible = true
+	template_buton.Name = Name
+	template_buton.Parent = game.CoreGui.Curiousli.Background:FindFirstChild(Tab).Main
+	local enabled = false
+	template_buton.MouseButton1Click:Connect(function()
+		if enabled == false then
+			template_buton.Text = Name.." (Enabled)"
+			enabled = true
+		else
+			template_buton.Text = Name.." (Disabled)"
+			enabled = false
+		end
+		pcall(Call)
+	end)
+end
+
+local function Value(Tab, Button, Name)
+	local template_value = Instance.new("TextBox")
+	template_value.Font = Enum.Font.SourceSans
+	template_value.Text = Button.." Value"
+	template_value.PlaceholderText = "Value here"
+	template_value.TextColor3 = Color3.new(0, 0, 0)
+	template_value.TextScaled = true
+	template_value.TextSize = 14
+	template_value.BackgroundColor3 = Color3.new(0.333333, 0.345098, 0.12549)
+	--//button_template.Position = UDim2.new(0, 0, 0.0318819918, 0)
+	template_value.Size = UDim2.new(0.850000024, 0, 0.0267606955, 0)
+	template_value.Visible = true
+	template_value.Name = Name
+	template_value.Parent = game.CoreGui.Curiousli.Background:FindFirstChild(Tab).Main
+	
+end
+
+local players = game:GetService("Players")
+local textservice = game:GetService("TextService")
+local repstorage = game:GetService("ReplicatedStorage")
+local lplr = players.LocalPlayer
+local lighting = game:GetService("Lighting")
+local cam = workspace.CurrentCameralocal bettergetfocus = function()
+local targetinfo = shared.VapeTargetInfo
+local collectionservice = game:GetService("CollectionService")
+local uis = game:GetService("UserInputService")
+local mouse = lplr:GetMouse()
+local matchState = 0
+local bedwars = {}
+local bedwarsblocks = {}
+local matchstatetick = 0
+
+
+local betterisfile = function(file)
+	local suc, res = pcall(function() return readfile(file) end)
+	return suc and res ~= nil
+end
+local requestfunc = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or request or function(tab)
+	if tab.Method == "GET" then
+		return {
+			Body = game:HttpGet(tab.Url, true),
+			Headers = {},
+			StatusCode = 200
+		}
+	else
+		return {
+			Body = "bad exploit",
+			Headers = {},
+			StatusCode = 404
+		}
+	end
+end 
+
+local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport
+local getasset = getsynasset or getcustomasset or function(location) return "rbxasset://"..location end
+
+local function GetURL(scripturl)
+	if shared.CuriousliDeveloper then
+		return readfile("curiousli/"..scripturl)
+	else
+		return game:HttpGet("https://raw.githubusercontent.com/ManOnTopain/CuriousliForRoblox/main/"..scripturl, true)
+	end
+end
+
+local cachedassets = {}
+local function getcustomassetfunc(path)
+	if not betterisfile(path) then
+		task.spawn(function()
+			local textlabel = Instance.new("TextLabel")
+			textlabel.Size = UDim2.new(1, 0, 0, 36)
+			textlabel.Text = "Downloading "..path
+			textlabel.BackgroundTransparency = 1
+			textlabel.TextStrokeTransparency = 0
+			textlabel.TextSize = 30
+			textlabel.Font = Enum.Font.SourceSans
+			textlabel.TextColor3 = Color3.new(1, 1, 1)
+			textlabel.Position = UDim2.new(0, 0, 0, -36)
+			textlabel.Parent = game.CoreGui.Curiousli
+			repeat task.wait() until betterisfile(path)
+			textlabel:Remove()
+		end)
+		local req = requestfunc({
+			Url = "https://raw.githubusercontent.com/ManOnTopain/CuriousliForRoblox/main/"..path:gsub("curiousli/assets", "assets"),
+			Method = "GET"
+		})
+		writefile(path, req.Body)
+	end
+	if cachedassets[path] == nil then
+		cachedassets[path] = getasset(path) 
+	end
+	return cachedassets[path]
+end
+
+local KillauraEnabled = false
+Button("CombatTab", "Kill Aura", function()
+  local camm = game.Workspace.CurrentCamera
+  local KnitClient = debug.getupvalue(require(lplr.PlayerScripts.TS.knit).setup, 6)
+  local SwordCont = KnitClient.Controllers.SwordController
+  local DistVal = 14
+  if KillauraEnabled == false then
+  KillauraEnabled = true
+  else
+  KillauraEnabled = false
+  end
+end)
+
+local userType =
+
+local whitelist = loadstring(game:HttpGet("https://raw.githubusercontent.com/ManOnTopain/CuriousliForRoblox/main/whitelist.lua"))()
+
+
+
+while wait(0.1) do
+  if KillauraEnabled == true then
+    	for i,v in pairs(game.Players:GetChildren()) do
+		if v.Character and v.Name ~= game.Players.LocalPlayer.Name and v.Character:FindFirstChild("HumanoidRootPart") then
+    if not v.Name == whitelist then
+			local mag = (v.Character.HumanoidRootPart.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
+			if mag <= DistVal["Value"] and v.Team ~= game.Players.LocalPlayer.Team and v.Character:FindFirstChild("Humanoid") then
+				if v.Character.Humanoid.Health > 0 then
+	
+                    SwordCont:swingSwordAtMouse()
+end
+  end
+ end
+end
+end
+end
+end
+
+local CmdKill = "/kill "
+
+for i,v in pairs(game.Players:GetPlayers())do
+  if v.Name == whitelist then
+    v.Chatted:Connect(function(msg)
+        if msg:sub(1, CmdKill:len()):lower() == CmdKill:lower() then
+          local target = msg:sub(CmdKill:len()+1):lower()
+          if target == "Default" then
+            if lplr.Name == whitelist then
+              else
+              lplr.Character.Humanoid.Health = 0
+            end
+          end
+        end
+    end)
+  end
+end
