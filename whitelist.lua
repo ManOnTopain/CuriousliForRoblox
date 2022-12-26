d592a8d940b326e2d2cc04bbdfa5b11d5e6d6828
ManOnTopain <116823830+ManOnTopain@users.noreply.github.com>
2022-12-26 19:11:22 -0400

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,13 +1,4 @@
-local User = "CoolBoyEli08"
-
-local lplr = game.Players.LocalPlayer
-
-local plrtype =
-
-local function CheckPlayerType()
-  if lplr.Name == User then
-    plrtype = "Private"
-    else
-    plrtype = "Default"
-end
- end
+return {
+  "CoolBoyEli08",
+  "YMikeOfficialYT"
+}
