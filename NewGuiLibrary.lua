230cfeb5d68d2f4d302b71c8926c2e13189af626
ManOnTopain <116823830+ManOnTopain@users.noreply.github.com>
2023-01-05 12:30:32 -0400

    Update NewGuiLibrary.lua

--- NewGuiLibrary.lua
+++ NewGuiLibrary.lua
@@ -108,3 +108,86 @@ local function getcustomassetfunc(path)
 	end
 end
 
+
+shared.GuiLibrary = {
+    ["Button"] = function(Tab, Name, Call)
+        local template_buton = Instance.new("TextButton")
+    template_buton.Font = Enum.Font.SourceSansBold
+    template_buton.Text = Name
+    template_buton.TextColor3 = Color3.new(0, 0, 0)
+    template_buton.TextScaled = true
+    template_buton.TextSize = 14
+    template_buton.TextWrapped = true
+    template_buton.BackgroundColor3 = Color3.new(0.333333, 0.345098, 0.12549)
+    --//template_buton.Position = UDim2.new(-0.0187500007, 0, 0.00350099197, 0)
+    template_buton.Size = UDim2.new(0.925000012, 0, 0.0291114338, 0)
+    template_buton.Visible = true
+    template_buton.Name = Name
+    template_buton.Parent = game.CoreGui.Curiousli.Background:FindFirstChild(Tab).Main
+    local enabled = false
+    template_buton.MouseButton1Click:Connect(function()
+        if enabled == false then
+            template_buton.Text = Name.." (Enabled)"
+            enabled = true
+        else
+            template_buton.Text = Name.." (Disabled)"
+            enabled = false
+        end
+        pcall(Call)
+    end)
+    end,
+    
+    ['Warn'] = function(Title, Text, Delay)
+        	
+local warn = Instance.new("Frame")
+warn.BackgroundColor3 = Color3.new(0.192157, 0.180392, 0.0784314)
+warn.BackgroundTransparency = 0.4000000059604645
+warn.Position = UDim2.new(0, 0, 0.480000019, 0)
+warn.Size = UDim2.new(1.00000012, 0, 0.227368414, 0)
+warn.Visible = true
+warn.Name = "Warn"
+warn.Parent = game.CoreGui.Curiousli.WarnList
+
+local uicorner = Instance.new("UICorner")
+uicorner.Parent = warn
+
+local uistroke = Instance.new("UIStroke")
+uistroke.Parent = warn
+
+local titletext = Instance.new("TextLabel")
+titletext.Font = Enum.Font.SourceSansBold
+titletext.Text = Title
+titletext.TextColor3 = Color3.new(0, 0, 0)
+titletext.TextScaled = true
+titletext.TextSize = 14
+titletext.TextWrapped = true
+titletext.BackgroundColor3 = Color3.new(1, 1, 1)
+titletext.BackgroundTransparency = 1
+titletext.Size = UDim2.new(0.999999881, 0, 0.296296299, 0)
+titletext.Visible = true
+titletext.Name = "TitleText"
+titletext.Parent = warn
+
+local texttext = Instance.new("TextLabel")
+texttext.Font = Enum.Font.SourceSansBold
+texttext.Text = Text
+texttext.TextColor3 = Color3.new(0, 0, 0)
+texttext.TextScaled = true
+texttext.TextSize = 14
+texttext.TextWrapped = true
+texttext.BackgroundColor3 = Color3.new(1, 1, 1)
+texttext.BackgroundTransparency = 1
+texttext.Position = UDim2.new(0.0235690214, 0, 0.398148149, 0)
+texttext.Size = UDim2.new(0.952861845, 0, 0.527777791, 0)
+texttext.Visible = true
+texttext.Name = "TextText"
+texttext.Parent = warn	
+task.wait(Delay)
+warn:Destroy()
+    end,
+	['UpdateValue'] = function(Tab, Name, NewName)
+		game.CoreGui.Curiousli.Background:FindFirstChild(Tab).Main:FindFirstChild(Name).Text = NewName
+	end
+}
+
+
